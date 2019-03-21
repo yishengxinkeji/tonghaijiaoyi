f22fa0ffa085294d748ee37c17e72aa58e4906c5
wangbowei <498434962@qq.com>
2019-03-21 10:44:06 +0800

    注册

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/interceptor/OneInterceptor.java
@@ -0,0 +1,32 @@
+package com.ruoyi.web.core.config.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OneInterceptor implements HandlerInterceptor {
+    /**
+     * 在请求处理之前进行调用（Controller方法调用之前）
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse
+            httpServletResponse, Object object) throws Exception {
+
+        System.out.println("=====================");
+
+        System.out.println("被one拦截...");
+
+        return true;
+    }
+
+
+    /**
+     * 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
+}
